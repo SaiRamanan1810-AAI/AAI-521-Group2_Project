220a76be9aa65fd827f49d6e4d3271fb5478ed68
SaiRamanan1810-AAI <smk@sandiego.edu>
2025-12-06 10:44:14 +0530

    Stream lit integration complete

--- notebooks/plantDiseasePredictionapp.py
+++ notebooks/plantDiseasePredictionapp.py
@@ -1,4 +1,41 @@
 import streamlit as st
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+
+model_tuned = tf.keras.models.load_model("..\\models\model_mobileNet_20Epochs_finetuned.h5")
+splitDest = "E:\\MS_USD\\Course 8_AAI-521_ComputerVision\\Final Project\\AAI-521-Group2_Project\\DatasetForDiseasePrediction\\ImagesSplitIntoTrain_Test"
+classNames = ['anthracnose',
+ 'bacterial blight',
+ 'brown spot',
+ 'green mite',
+ 'gumosis',
+ 'leaf blight',
+ 'leaf curl',
+ 'leaf miner',
+ 'leaf spot',
+ 'mosaic',
+ 'red rust',
+ 'septoria leaf spot',
+ 'streak virus',
+ 'verticulium wilt']
+
+def predictDisease(imgPath, ind):
+    # img_path = splitDest + imgPath
+    img_path = imgPath
+    img = Image.open(img_path)
+    img = img.resize((224, 224))
+    img_array = np.array(img)
+    img_array = np.expand_dims(img_array, axis=0)    
+    pred = model_tuned.predict(img_array)
+    classNameInd = np.argmax(pred[0])
+    # print(classNameInd)
+    confidence = round(np.max(pred[0]),4)
+    print(classNames[classNameInd],confidence)
+    # plt.subplot(5,3,ind)
+    # plt.imshow(img)
+    # plt.title(f"{classNames[classNameInd]}, Confidence Score: {confidence*100} %")
+    return classNames[classNameInd],confidence
 
 st.set_page_config(
     page_title="Plant Disease Prediction App",
@@ -37,11 +74,14 @@ st.sidebar.markdown("**Powered by:** Streamlit + Computer Vision")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    
+    print(uploaded_file)
     image = Image.open(uploaded_file)
-
-    st.image(image, caption="Uploaded Image", use_column_width=True)
-
+    image = image.resize((224,224))
+    st.image(image, caption="Uploaded Image", use_column_width=False)
+    diseaseClass, conf = predictDisease(uploaded_file, 0)
+    st.header(f"**Disease Identified** : {diseaseClass}")
+    st.write(f"**Confidence score** : {conf*100} %")
+    st.write(f"**CNN Model Used** : MobileNetV2")
     st.subheader("📊 Image Details")
     st.write(f"**Format:** {image.format}")
     st.write(f"**Size (pixels):** {image.size[0]} x {image.size[1]}")
